d6bbf34936d4eb21d82e31068f6ec36249c2a02e
Vitor Chaves <chavesvb21@gmail.com>
2026-05-19 10:45:28 -0300

    criação aula 2

---
+++ javascript/aula2/main.js
@@ -0,0 +1,12 @@
+let idade = 6
+if (idade >=18){
+    console.log("maior de idade")
+}
+else if(idade < 18 && idade >=11){
+    console.log("adolescente")
+}
+else{
+    console.log("crianca")
+}
+
+console.log(2==="2")
