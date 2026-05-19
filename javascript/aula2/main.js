dd81b31ee403be8c9cbcb4e75908fa60b13ff4ab
Vitor Chaves <chavesvb21@gmail.com>
2026-05-19 10:59:50 -0300

    atualização aula 2

--- javascript/aula2/main.js
+++ javascript/aula2/main.js
@@ -1,12 +1,21 @@
-let idade = 6
-if (idade >=18){
-    console.log("maior de idade")
-}
-else if(idade < 18 && idade >=11){
-    console.log("adolescente")
+// let idade = 6
+// if (idade >=18){
+//     console.log("maior de idade")
+// }
+// else if(idade < 18 && idade >=11){
+//     console.log("adolescente")
+// }
+// else{
+//     console.log("crianca")
+// }
+
+// console.log(2==="2")
+
+
+let nome = prompt("digite o nome")
+if(nome !== "" && nome !== null){
+    console.log("ola",nome)
 }
 else{
-    console.log("crianca")
+    console.log("sem valor no prompt")
 }
-
-console.log(2==="2")
